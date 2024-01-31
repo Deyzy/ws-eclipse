0fb26d2f588c8dc9cb168ab2ee3f50b644ece579
Deyzy <deisesantos487@gmail.com>
2024-01-30 21:08:25 -0300

    new code

---
+++ repeticao/src/repeticao/Main.java
@@ -0,0 +1,23 @@
+package repeticao;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int x = sc.nextInt();
+		
+		int soma = 00;
+		while (x != 0){
+			soma = soma + x; //soma+=x 
+			x = sc.nextInt();
+		}
+		System.out.println(soma);
+		
+		sc.close();
+	}
+
+}
