7fa42b7b362ab5881c937770e3a9dd3de2f6b758
Deyzy <deisesantos487@gmail.com>
2024-05-18 18:36:37 -0300

    exercicios
    novo exercicios adicionados com a estrutura for

--- Exercicios04/src/Tabuada.java
+++ Exercicios04/src/Tabuada.java
@@ -1,8 +1,18 @@
+import java.util.Scanner;
 
 public class Tabuada {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		for (int i=1; i<=10; i++) {
+			int produto = i * n;
+			System.out.println(i + " x " + n + " = " + produto);
+			
+		}
+		sc.close();
 
 	}
 
